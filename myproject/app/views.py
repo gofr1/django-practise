eadba6a130a72735ab972e03731a892481f5ea30
Ilia Odintsov <gofr.one@gmail.com>
2021-04-01 11:34:11 +0300

    Organizing URLs

--- myproject/app/views.py
+++ myproject/app/views.py
@@ -1,5 +1,22 @@
 from django.http import HttpResponse
+from datetime import date
 
 def hello(request):
-   text = """<h1>welcome to my app !</h1>"""
+   text = """<h1>Welcome to my app!</h1>"""
+   return HttpResponse(text)
+
+def viewNumber(request, number):
+   text = f'Number: {number}'
+   return HttpResponse(text)
+
+def viewDate(request, year, month, day):
+   try:
+      y, m, d = int(year), int(month), int(day)
+      input_date = date(y, m, d)
+      text = f'Input: {year}-{month}-{day}'
+   except ValueError as v:
+      if hasattr(v, 'message'):
+         text = v.message
+      else:
+         text = 'Incorrect date'
    return HttpResponse(text)
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from rest_framework.routers import SimpleRouter
 
 from post.views import PostViewSet
-from app.views import hello
+import app.views as av
 
 router = SimpleRouter()
 router.register('posts', PostViewSet)
@@ -29,5 +29,6 @@ urlpatterns = [
     path('appstaff/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
-    path('hello/', hello),
+    path('hello/', av.hello),
+    re_path(r'^number/(\d+)/', av.viewNumber),
 ]
