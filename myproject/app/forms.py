c50514fe821f2fbf5c11c6674fe5e17924b556ee
Ilia Odintsov <gofr.one@gmail.com>
2021-04-12 19:54:03 +0300

    Login form, view and urls

--- myproject/app/forms.py
+++ myproject/app/forms.py
@@ -12,3 +12,7 @@ class NameForm(forms.Form):
 class PictureUploadForm(forms.Form):
    name = forms.CharField(max_length = 100)
    picture = forms.ImageField()
+
+class LoginForm(forms.Form):
+   username = forms.CharField(max_length = 100)
+   password = forms.CharField(widget = forms.PasswordInput())
