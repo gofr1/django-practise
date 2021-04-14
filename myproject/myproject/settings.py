4624b8f67a31c40352574b138d9f700ed5f5896e
Ilia Odintsov <gofr.one@gmail.com>
2021-04-14 19:37:41 +0300

    Define STATICFILES_FINDERS

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -123,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = BASE_DIR / 'static/'
 STATIC_URL = '/static/'
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 MEDIA_ROOT = BASE_DIR / 'media/'
 MEDIA_URL = '/media/'
